aebad2379ed2eb8832c7a0f330c9a0e3ea141346
Eric Nova <ericnova23@hotmail.com>
2025-01-21 02:41:51 -0600

    S5 - Factory Method - Tarea

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,95 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class Cheeseburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cCheeseburger", COLORS.yellow);
+  }
+}
+
+class DoubleCheeseburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cDouble Cheeseburger", COLORS.brown);
+  }
+}
+
+class VeggieBurger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cVeggie Burger", COLORS.green);
+  }
+}
+
+class ChickenBurger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cChicken Burger", COLORS.orange);
+  }
+}
+
+abstract class Restaurant {
+  abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class CheeseburgerStore extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new Cheeseburger();
+  }
+}
+
+class DoubleCheeseburgerStore extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new DoubleCheeseburger();
+  }
+}
+
+class VeggieBurgerStore extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new VeggieBurger();
+  }
+}
+
+class ChickenBurgerStore extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new ChickenBurger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+
+  const burgerType = prompt(
+    "What type of burger do you want? (1) Cheeseburger (2) Double Cheeseburger (3) Veggie Burger (4) Chicken Burger"
+  );
+
+  switch (burgerType) {
+    case "1":
+      restaurant = new CheeseburgerStore();
+      break;
+    case "2":
+      restaurant = new DoubleCheeseburgerStore();
+      break;
+    case "3":
+      restaurant = new VeggieBurgerStore();
+      break;
+    case "4":
+      restaurant = new ChickenBurgerStore();
+      break;
+    default:
+      throw new Error("Invalid burger type");
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
