361b4eda1528da28f551ed4462914110b877781b
Eric Nova <ericnova23@hotmail.com>
2025-01-31 11:38:46 -0600

    S6 - Abstract Factory - Tarea

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -1,3 +1,4 @@
+
 /**
  * ! Abstract Factory:
  * Es un patrón de diseño que permite crear familias de objetos relacionados
@@ -12,8 +13,82 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  *  El propósito del Abstract Factory es crear familias de objetos relacionados
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Hamburger {
+    prepare(): void;
+}
+
+interface Drink {
+    pour(): void;
+}
+
+class ChickenBurger implements Hamburger {
+    prepare(): void {
+        console.log('Making %cChicken Burger', COLORS.yellow);
+    }
+}
+
+class BeefBurger implements Hamburger {
+    prepare(): void {
+        console.log('Making %cBeef Burger', COLORS.brown);
+    }
+}
+
+class Coke implements Drink {
+    pour(): void {
+        console.log('Pouring %cCoke', COLORS.red);
+    }
+}
+
+class Water implements Drink {
+    pour(): void {
+        console.log('Pouring %cWater', COLORS.blue);
+    }
+}
+
+interface RestaurantFactory {
+    createHamburger(): Hamburger;
+    createDrink(): Drink;
+}
+
+class FastFoodRestaurant implements RestaurantFactory {
+    createHamburger(): Hamburger {
+        return new ChickenBurger();
+    }
+
+    createDrink(): Drink {
+        return new Coke();
+    }
+}
+
+class FancyRestaurant implements RestaurantFactory {
+    createHamburger(): Hamburger {
+        return new BeefBurger();
+    }
+
+    createDrink(): Drink {
+        return new Water();
+    }
+}
+
+function main(factory: RestaurantFactory) {
+    const hamburger = factory.createHamburger();
+    const drink = factory.createDrink();
+
+    hamburger.prepare();
+    drink.pour();
+}
+
+console.log('%cFast Food Restaurant:', COLORS.green);
+
+main(new FastFoodRestaurant());
+
+console.log('%cFancy Restaurant:', COLORS.violet);
+main(new FancyRestaurant());
