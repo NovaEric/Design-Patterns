3175d0990d434873bddfd347deaf721fba84fe32
Eric Nova <ericnova23@hotmail.com>
2025-01-31 17:15:37 -0600

    S7 - Prototype - Solución a la tarea

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,42 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Document {
+    public title: string;
+    private content: string;
+    public author: string;
+
+    constructor(title: string, content: string, author: string) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    clone(): Document {
+        return new Document(this.title, this.content, this.author);
+    }
+
+    displayInfo(): void {
+        console.log(`
+            Title: ${this.title}
+            Content: ${this.content}
+            Author: ${this.author}`);
+    }
+}
+
+function main() {
+    console.log('\n%cDocument Prototype 1', COLORS.cyan);
+    const document = new Document('Design Patterns', 'Creational Patterns', 'GOF');
+    document.displayInfo();
+    
+    const documentClone = document.clone();
+    documentClone.title = 'Design Patterns Clone';
+    
+    console.log('\n%cDocument Prototype 2', COLORS.green);
+    documentClone.displayInfo();
+}
+
+main();
