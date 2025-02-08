2f811db2a74301e2cc3d1fc72967ab9f70217394
Eric Nova <ericnova23@hotmail.com>
2025-02-08 03:01:12 -0600

    S10 - Factory Function - Utilización

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * ! Factory Function
  * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
@@ -7,3 +9,28 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+type Language = 'es' | 'en' | 'fr';
+
+function createGreeter (lang: Language){
+    return function (name: string) {
+        const message = {
+            es: `Hola, %c${name}!`,
+            en: `Hello, %c${name}!`,
+            fr: `Bonjour, %c${name}!`
+        }
+
+        return console.log(message[lang], COLORS.green);
+    }
+}
+
+function main() {
+    const es = createGreeter('es');
+    const en = createGreeter('en');
+    const fr = createGreeter('fr');
+
+    es('Eric');
+    en('Eric');
+    fr('Eric');
+}
+
+main();
