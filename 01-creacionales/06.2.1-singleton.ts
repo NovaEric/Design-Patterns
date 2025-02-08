4c4b1449505662bb57b0d7d9c241f9ad491d5b33
Eric Nova <ericnova23@hotmail.com>
2025-02-08 02:37:56 -0600

    S9 Done

--- 01-creacionales/06.2.1-singleton.ts
+++ 01-creacionales/06.2.1-singleton.ts
@@ -1,3 +1,6 @@
+import { COLORS } from "../helpers/colors.ts";
+import { configManager } from "./06.2.1-config-manager.ts";
+
 /**
  * ! Singleton:
  * Es un patrón de diseño creacional que garantiza que una clase
@@ -9,3 +12,11 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+configManager.setConfig('123', '123');
+configManager.setConfig('456', '456');
+configManager.setConfig('789', '789');
+console.log(`%c${configManager.getConfig('456')}`, COLORS.blue);
+console.log(`%c${configManager.getConfig('123')}`, COLORS.purple);
+console.log(`%c${configManager.getConfig('789')}`, COLORS.green);
+console.log(`%c${JSON.stringify(configManager.getAllConfig())}`, COLORS.red);
+console.log(configManager.getAllConfig());
