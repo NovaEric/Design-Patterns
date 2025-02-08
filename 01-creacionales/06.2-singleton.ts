63152b1a58f49e8b0e0105ba3e574a5b633c45bf
Eric Nova <ericnova23@hotmail.com>
2025-02-08 02:15:10 -0600

    S9 - Singleton - Solución a la tarea

--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -15,22 +15,43 @@ class DatabaseConnection {
   private connected: boolean = false;
 
   // Constructor privado para evitar instancias directas
-  private constructor() {}
+  private constructor() {
+    this.connected = false;
+  }
 
   // Método estático para obtener la instancia única
   public static getInstance(): DatabaseConnection {
     // Completar: implementar el patrón Singleton
-    throw new Error('Method not implemented.');
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+      console.log('%cA new connection has been stablished', COLORS.blue);
+    }
+
+    return DatabaseConnection.instance;
   }
 
   // Método para conectar a la base de datos
   public connect(): void {
     // Completar: si no está conectado, mostrar mensaje de conexión
+    if (!this.connected) {
+      this.connected = true;
+      console.log('%cConnection is ON.', COLORS.green);
+      return;
+    }
+    console.log('%cAlready connected.', COLORS.green);
+    
   }
-
+  
   // Método para desconectar de la base de datos
   public disconnect(): void {
     // Completar: desconectar y mostrar mensaje de desconexión
+    if (this.connected) {
+      this.connected = false;
+      console.log('%cConnection is OFF.', COLORS.red);
+      return;
+    }
+    console.log('%cAlready disconnected.', COLORS.red);
+
   }
 }
 
