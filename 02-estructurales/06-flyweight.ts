e734bf142167e0e2b5d7c58df79d647b2b58b782
Eric Nova <ericnova23@hotmail.com>
2025-02-24 22:19:55 -0600

    S18

--- 02-estructurales/06-flyweight.ts
+++ 02-estructurales/06-flyweight.ts
@@ -8,3 +8,67 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Location {
+    display(coordinates: { x: number; y: number }): void;
+}
+
+class LocationIcon implements Location {
+    private type: string;
+    private iconImage: string;
+
+    constructor(type: string, iconImage: string) {
+        this.type = type;
+        this.iconImage = iconImage;
+    }
+
+    display(coordinates: { x: number; y: number; }): void {
+        console.log(
+            `Corrdinates: ${coordinates.x}, ${coordinates.y} - Type: ${this.type} - Icon: %c[${this.iconImage}]`,
+            COLORS.green
+        )
+    }
+}
+class LocationFactory {
+    private icons: Record<string, LocationIcon> = {};
+    
+    getLocationIcon(type: string): LocationIcon {
+        if (!this.icons[type]) {
+            console.log(`%cCreating icon: ${type}`, COLORS.yellow);
+            const iconImage = `icon-${type.toLowerCase()}.png`;
+            this.icons[type] = new LocationIcon(type, iconImage);
+        }
+        return this.icons[type];
+    }
+}
+
+class MapLocation {
+    private coordinates: { x: number; y: number; };
+    private icon: LocationIcon;
+
+    constructor(x: number, y: number, icon: LocationIcon) {
+        this.coordinates = { x, y };
+        this.icon = icon;
+    }
+
+    display(): void {
+        this.icon.display(this.coordinates);
+    }
+}
+
+function main() {
+    const factory = new LocationFactory();
+    const locations: MapLocation[] = [];
+
+    locations.push(new MapLocation(10, 20, factory.getLocationIcon('restaurant')));
+    locations.push(new MapLocation(15, 25, factory.getLocationIcon('restaurant')));
+    locations.push(new MapLocation(20, 30, factory.getLocationIcon('hospital')));
+    locations.push(new MapLocation(25, 35, factory.getLocationIcon('restaurant')));
+    locations.push(new MapLocation(30, 40, factory.getLocationIcon('hospital')));
+
+    locations.forEach(location => location.display());
+}
+
+main();
